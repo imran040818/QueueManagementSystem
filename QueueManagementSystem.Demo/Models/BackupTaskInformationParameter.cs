d4bc1bcebf7019684e9eedd38da317aac294f24e
imran <imran040818@gmail.com>
2021-11-28 19:14:12 +0530

    added git ignore file

---
+++ QueueManagementSystem.Demo/Models/BackupTaskInformationParameter.cs
@@ -0,0 +1,9 @@
+﻿using QueueManagementSystem.Models;
+
+namespace QueueManagementSystem.Demo.Models
+{
+    public class BackupTaskInformationParameter : TaskInformationParameter
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ QueueManagementSystem.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueueManagementSystem.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
