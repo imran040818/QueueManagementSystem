6b1f4738545793b957f02d02dae8d5808c5f6594
imran <imran040818@gmail.com>
2021-12-09 00:39:35 +0530

    * QueueEventArg.cs: Refreshed the git ignore file and updated the   version
    * .gitignore:
    * UserPrefs.xml:
    * project.nuget.cache:
    * project.assets.json:
    * QueueManagementSystem.csproj:
    * QueueManagementSystem.csproj.nuget.g.props:
    * QueueManagementSystem.csproj.nuget.g.targets:
    * QueueManagementSystem.pdb:
    * QueueManagementSystem.dll:
    * QueueManagementSystem.pdb:
    * QueueManagementSystem.dll:
    * QueueManagementSystem.csproj.nuget.dgspec.json:
    * QueueManagementSystem-Debug.json:
    * QueueManagementSystem.deps.json:
    * QueueManagementSystem.assets.cache:
    * QueueManagementSystem.AssemblyInfo.cs:
    * .NETCoreApp,Version=v3.1.AssemblyAttributes.cs:
    * QueueManagementSystem.AssemblyInfoInputs.cache:
    * QueueManagementSystem.csproj.FileListAbsolute.txt:
    * QueueManagementSystem.csproj.CoreCompileInputs.cache:

---
+++ QueueManagementSystem/Events/EventArg/QueueEventArg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace QueueManagementSystem.Events.EventArg
+{
+    public class QueueEventArg<T> : EventArgs
+    {
+        public QueueEventArg(T data)
+        {
+            Data = data;
+        }
+        public QueueEventArg(T data, Exception exception)
+        {
+            Data = data;
+            Exception = exception;
+        }
+        public T Data { get; set; }
+        public Exception Exception { get; }
+    }
+}
